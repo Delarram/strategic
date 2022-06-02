1327fe5481cf4f539c0920abed73e4c282baf11f
Delarram <Delurram@gmail.com>
2022-06-02 14:50:07 +0630

    added detail on detail screen

--- lib/widget/detail.dart
+++ lib/widget/detail.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'product.dart';
@@ -8,6 +9,36 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xffFFFFFF),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height*20.0,
+            width: double.infinity,
+            child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              child: ClipRRect(
+                child: CachedNetworkImage(
+                  height: double.infinity,
+                  fit: BoxFit.fill,
+                  imageUrl:
+                  "https://media.istockphoto.com/photos/business-partnership-business-man-investor-handshake-with-effect-picture-id1313742092?s=612x612",
+                  progressIndicatorBuilder: (context,
+                      url, downloadProgress) =>
+                      CircularProgressIndicator(
+                          value: downloadProgress
+                              .progress),
+                  errorWidget:
+                      (context, url, error) =>
+                  const Icon(Icons.error),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
