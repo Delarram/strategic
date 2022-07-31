284910fb29e728f5f8db5d61c71f50aa8eb6f8fb
Delarram <Delurram@gmail.com>
2022-07-31 18:52:38 +0630

    added dataagent and api constant

---
+++ lib/network/api_constant.dart
@@ -0,0 +1,16 @@
+
+
+///BaseUrl
+const String baseUrl = "api.themoviedb.org";
+
+///End_points
+const String endPointGetPlaying = "3/movie/now_playing";
+
+///parameters
+const String paramGetNowPlaying ="api_key";
+const String paramLanguage ="language";
+const String paramPage="page";
+
+///constant values
+const String apiKey = "73e7250e0374fbd414712f1c45bf6505";
+const String languageEnUs = "en-US";
