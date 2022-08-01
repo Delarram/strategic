e530c7d9247325a9b6fc0216a56a3c94172f29f0
Delarram <Delurram@gmail.com>
2022-08-01 15:23:41 +0630

    updated retrofitdataagentimpl

--- lib/network/retrofit_data_agent_impl.dart
+++ lib/network/retrofit_data_agent_impl.dart
@@ -7,22 +7,28 @@ import 'package:strategic/network/the_movie_api.dart';
 
 class RetrofitDataAgentImpl extends MovieDataAgent{
    late TheMovieApi mApi;
-
+//before singleton
   RetrofitDataAgentImpl(){
     final dio = Dio();
      mApi = TheMovieApi(dio);
   }
+  //after singleton
 
   @override
   void getNowPlayingMovies(int page) {
 
     mApi.
     getNowPlayingMovies(apiKey,languageEnUs,page.toString())
-        .then((response) => {
-      response.results!.forEach((movie) => debugPrint(movie.toString()))
-    }).catchError((error){
-      debugPrint("Now Playing Movies========> ${error.toString()}");
-    });
+        .asStream()
+        .map((event) => event.results)
+        .first;
+
+
+    //     .then((response) => {
+    //   response.results!.forEach((movie) => debugPrint(movie.toString()))
+    // }).catchError((error){
+    //   debugPrint("Now Playing Movies========> ${error.toString()}");
+    // });
   }
 
 }
