81ee980b3290d5108298bc2e6bc37e257464acb7
Delarram <Delurram@gmail.com>
2022-08-04 15:23:49 +0630

    tip

--- lib/tipsfordart/enum.dart
+++ lib/tipsfordart/enum.dart
@@ -4,7 +4,9 @@ void main(){
  print(city.index);//if u wanna show with index which included in City
  print(city.name);//if u wanna show with name which in City but its depend on index
  print(City.values);//if u wanna show all values which included in City
+
 }
+
 
 enum City{
   mogok,
@@ -12,3 +14,4 @@ enum City{
   yangon,
   nayPyiDaw
 }
+
