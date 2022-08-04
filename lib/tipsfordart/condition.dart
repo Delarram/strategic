dfa686c24a5728e1aa6ef3d273445602e68b4c0a
Delarram <Delurram@gmail.com>
2022-08-04 17:04:03 +0630

    tips

---
+++ lib/tipsfordart/condition.dart
@@ -0,0 +1,11 @@
+void main(){
+  var number=1;
+
+  for(var i; i<10; i++){
+    print (number);
+    number++;
+  }
+  for (var i=0; i<20; i++){
+
+  }
+}
--- lib/tipsfordart/enum.dart
+++ lib/tipsfordart/enum.dart
@@ -1,13 +1,35 @@
+
+
 void main(){
- City city = City.yangon;
+  //traditional loop
+  for(var i=1; i <= 20 ; i ++) {
+    print(i);
 
- print(city.index);//if u wanna show with index which included in City
- print(city.name);//if u wanna show with name which in City but its depend on index
- print(City.values);//if u wanna show all values which included in City
-
-}
+  }
 
 
+
+ // City city = City.mogok;
+ //
+ // print(city.index);//if u wanna show with index which included in City
+ // print(city.name);//if u wanna show with name which in City but its depend on index
+ // print(City.values);//if u wanna show all values which included in City
+ //
+ // switch (city){
+ //   case City.mogok: {
+ //     print("this is mogok");
+ //     break;
+ //   }
+ //   case City.mandalay:
+ //   print("hello");
+ //     break;
+ //   case City.yangon:
+ //     print("hello");
+ //     break;
+ //   case City.nayPyiDaw:
+ //     print("hello");
+ //     break;
+ }
 enum City{
   mogok,
   mandalay,
